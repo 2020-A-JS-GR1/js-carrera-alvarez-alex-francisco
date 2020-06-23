f544081fec41fb16a28b6d183f0979f0a1b719f1
Alex Carrera <a_fcalvarez@hotmail.com>
2020-06-23 16:42:39 -0500

    some every

--- 01-javascript/03-operadores.js
+++ 01-javascript/03-operadores.js
@@ -137,3 +137,32 @@ const respuestaFilter = arreglo
     );
 console.log('respuestaFilter',respuestaFilter);
 console.log('arreglo', arreglo);
+
+//Hay notas menores a 9? SI o NO
+// SOME devuelve un booleano
+//OR
+
+const respuestaSome = arreglo
+    .some(
+        (valorActual,indiceActual, arregloCompleto) => {
+            return valorActual.nota < 9;
+        }
+);
+
+console.log("respuestaSome", respuestaSome);
+
+//every
+//Devuelve un booleano
+//Todas las notas son mayores a 14
+//AND
+
+const respuestaEvery = arreglo
+    .every(
+        (valorActual,indiceActual, arregloCompleto) => {
+            return valorActual.nota > 14;
+        }
+    );
+
+console.log("respuestaEvery", respuestaEvery);
+
+
