2855b8c1a1cc041ad61c0ac584b22881043726f5
Alex <a_fcalvarez@hotmail.com>
2020-07-07 18:28:32 -0500

    inquire

--- 01-javascript/10-async-await.js
+++ 01-javascript/10-async-await.js
@@ -1,18 +1,39 @@
-const promesaLeerArchivo = new Promise();
-const promesaEscribirArchivo = new Promise();
+const promesaLeerArchivo = () => {
+    return new Promise(
+        (res,rej)=>{
+            res('CONTENIDO LEER ARCHIVO')
+        }
+    );
+}
+const promesaEscribirArchivo = () => {
+    return new Promise(
+        (res,rej) => {
+            rej('ERROR =(')
+        }
+    );
+}
 
 //ASYNC AWAIT -> DENTRO DE UNA FUNCION
 async function ejercicio() {
     console.log('1');
     try{
+        console.log('2');
         const respuestaLecturaArchivo = await promesaLeerArchivo();
+        console.log(respuestaLecturaArchivo);
+        console.log('3');
         await promesaEscribirArchivo(); //
+        console.log('4');
         const nuevoContenido = await promesaLeerArchivo();
         console.log(nuevoContenido);
+        console.log('5');
     } catch (error) {
         console.error(error);
     }
+    console.log('6');
+    console.log('7');
 }
 
-const f = async () => {
-}
+const respuestaEjercicios = ejercicio();
+//console.log('respuestaEjecicio', respuestaEjercicios);
+//console.log('promesaLeerArchivo', promesaLeerArchivo());
+//console.log('promesaEscribirArchivo', promesaEscribirArchivo());
